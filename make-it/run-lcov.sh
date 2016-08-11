cc1e9dbfeb4b71d75d73cde84d4f49dd7286c459
Devin Carr <devin.carr0@gmail.com>
2016-08-11 16:08:18 -0500

    possible fix

--- make-it/run-lcov.sh
+++ make-it/run-lcov.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Needed because travisci appends '-v' and causes it to break lcov
-lcov --base-directory ./objs --directory ./objs --gcov-tool ./llvm-gcov.sh --capture -o cov.info 1> /dev/null
+exec lcov --base-directory ./objs --directory ./objs --gcov-tool ./llvm-gcov.sh --capture -o cov.info 1> /dev/null
